06ef829c623ee13616d0381a472405078b739ae4
jasmine <jasminetripti5@gmail.com>
2021-06-04 22:19:21 +0600

    string-lengyh

---
+++ string_length.js
@@ -0,0 +1,16 @@
+var str = 'i am good'
+var length = 0
+
+while(true){
+    if (str.charAt(length) == '' ){
+        break
+    }
+    else{
+        length ++
+    }
+}
+
+console.log(length)
+var n = 'govt scholl'
+console.log(n.length)
+console.log('vghyghhgrfdrhh'.length)
