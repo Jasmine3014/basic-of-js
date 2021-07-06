197a8c180c07e08730938b60733fb28dda517677
jasmine <jasminetripti5@gmail.com>
2021-07-06 22:23:38 +0600

    literal-constractor

---
+++ literal_constractor.js
@@ -0,0 +1,23 @@
+//litarel
+var point = {
+    x:10 , y:50
+}
+ point.z=10
+console.log (point)
+
+//obj constructor
+var obj = new Object()
+obj.x=10
+console.log(obj)
+
+var obj2 = new Object()
+obj2.x=18
+console.log(obj2)
+
+//accessing in obj
+var ac1= {a:10,
+b:3 }
+console.log(ac1.a+ac1.b)
+
+//array notation
+console.log(ac1['a'])
