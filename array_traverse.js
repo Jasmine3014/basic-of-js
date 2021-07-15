ac4d8c25001d2385cd54cdd9ba38aa9e0c0210ec
jasmine <jasminetripti5@gmail.com>
2021-07-15 22:10:15 +0600

    array-traverse

---
+++ array_traverse.js
@@ -0,0 +1,11 @@
+var array = [1,3,45,7,4,66]
+
+for(i = 0; i < array.length; i++){
+    console.log(array[i])
+}
+
+// var sum = 0
+// for(var i = 0; i < array.length; i++){
+//     sum += array[i]
+// }
+// console.log(sum)
