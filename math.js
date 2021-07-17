fccf9940480497c3ed2dbcbf257a0d6aa743c3e6
jasmine <jasminetripti5@gmail.com>
2021-07-17 22:11:40 +0600

    math-function

---
+++ math.js
@@ -0,0 +1,27 @@
+//math part
+console.log(Math.E)
+console.log(Math.PI)
+
+var n = 4.894
+console.log(Math.abs(n))
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+
+//maximum or minimum
+console.log(Math.max(400,500))
+console.log(Math.min(100,500))
+console.log(Math.pow(4,5))
+console.log(Math.sqrt(4))
+
+// random number (everytime diff number by run the code)
+console.log(Math.random())
+console.log (Math.round(Math.random() *50 +1))
+
+
+
+
+
+
+
+
