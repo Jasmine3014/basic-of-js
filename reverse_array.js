eccae5b5265d57d41ee7881153503f58f2b908f6
jasmine <jasminetripti5@gmail.com>
2021-07-21 22:13:40 +0600

    array-reverse

---
+++ reverse_array.js
@@ -0,0 +1,9 @@
+var arr=[4, 8, 9, 30]
+// for(var i=0; i< (arr.length/2); i++ ){
+//     var temp = arr[i] //arrays value store
+//     arr[i]= arr[arr.length -1 -i]//  last arrays value set
+//     arr[arr.length -1 -i] = temp
+    
+//     }
+// console.log(arr)
+    console.log (arr.reverse())
